75e9087692c74995a7a52c68ad1754071ac5a6df
Maja Linder <guskalmas@student.gu.se>
2022-11-15 15:38:19 +0100

    #65 Set domain values of the scale

--- src/cumulativeLine.ts
+++ src/cumulativeLine.ts
@@ -15,9 +15,14 @@ export function renderCumulativeLine(pareto: Pareto) {
     // store the x and y positions for the line
     let positions: number[][] = [];
 
+    // store these to use later when setting the domain of the scale
+    let categoryPositions: number[] = [];
+    let cumulativePercentages: number[] = [];
+
     pareto.stackedBars.forEach(stackedBar => {
         positions.push([stackedBar.position * (100 / pareto.stackedBars.length), stackedBar.cumulativePercentage]);
-        
+        categoryPositions.push(stackedBar.position * (100 / pareto.stackedBars.length));
+        cumulativePercentages.push(stackedBar.cumulativePercentage);
     });
 
     let svg = d3.select("svg")
@@ -27,8 +32,8 @@ export function renderCumulativeLine(pareto: Pareto) {
     const height = svgContainer?.getBoundingClientRect().height as number;
     
 
-    var categoryScale:any = d3.scaleLinear().domain([0, 100]).range([0, width]),
-        valueScale:any = d3.scaleLinear().domain([0, 100]).range([height, 0]);
+    var categoryScale:any = d3.scaleLinear().domain([0, categoryPositions[categoryPositions.length -1]]).range([0, width]),
+        valueScale:any = d3.scaleLinear().domain([0, cumulativePercentages[cumulativePercentages.length -1]]).range([height, 0]);
 
     var line = d3.line<any>()
         .x(function (d) { return categoryScale(d[0]); })
@@ -40,6 +45,6 @@ export function renderCumulativeLine(pareto: Pareto) {
         .attr("transform", "translate(" + 0 + "," + 0 + ")")
         .attr("d", line)
         .style("fill", "none")
-        .style("stroke", "#CC0000")
+        .style("stroke", "#4916ea")
         .style("stroke-width", "2")
 }
