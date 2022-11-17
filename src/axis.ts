92008e42d994b24960bda37f3ca6f6565995d921
mohzan <mohzan@student.chalmers.se>
2022-11-17 11:59:22 +0100

    #90 refactoring scales and stackedBars

--- src/axis.ts
+++ src/axis.ts
@@ -5,40 +5,18 @@ import { Pareto} from "./pareto";
     d3.selectAll("path").remove();
     d3.selectAll("g").remove();
 
-    const paretoCategoryValues:any = [];
-    pareto.stackedBars.forEach((p) => {
-        paretoCategoryValues.push(p.label);
-    });
+    const paretoCategoryValues:string[] = moduleCategories(pareto)
 
-    let ticks:number = 0;
-    if(pareto.maxValue <= 50){
-        ticks = 5;
-    }else if (pareto.maxValue >50 && pareto.maxValue <= 100){
-        ticks= 10;
-    }else {
-        ticks= 20;
-    }
+    let ticks=moduleTicks(pareto)
     var svg = d3.select("svg");
     const svgBoundingClientRect:any = document.querySelector("#svg");
     const valueInPixels:any = svgBoundingClientRect.getBoundingClientRect();
 
-
     var svg = d3.select("svg");
-
-    var categoryAxis = d3
-            .scaleBand()
-            .domain(paretoCategoryValues)           
-            .range([0, valueInPixels.width - 100]),
-        valueAxis = d3
-            .scaleLinear()
-            .domain([0, pareto.maxValue]).nice(ticks)
-            .range([valueInPixels.height - 50, 100])
-            ,
-        percentageAxis = d3
-            .scaleLinear()
-            .domain([0 , 100 ])
-            .range([valueInPixels.height - 50, 100]);
-     
+    const categoryAxis= moduleCategoryAxis(paretoCategoryValues,valueInPixels.width);
+    const valueAxis = moduleValueAxis(pareto.maxValue,valueInPixels.height,ticks);
+    const percentageAxis =  modulePercentageAxis(valueInPixels.height)
+   
     var g = svg
         .append("g") 
         .attr("transform", "translate(" + 65 + "," + 0 + ")");
@@ -55,5 +33,49 @@ import { Pareto} from "./pareto";
         .tickFormat(function(d) {
           return d + "%";
         }));
-        
 }
+
+const moduleCategoryAxis =(domain:any,rangeWidth:number)=>{
+
+    let categoryAxis = d3
+            .scaleBand()
+            .domain(domain)           
+            .range([0, rangeWidth - 100])
+            .paddingInner(0.13)
+            .paddingOuter(0.26)
+    return(categoryAxis);
+
+}
+const moduleValueAxis = (domain:any,rangeHeight:number,ticks:number)=>{
+    let valueAxis = d3
+            .scaleLinear()
+            .domain([0, domain]).nice(ticks)
+            .range([rangeHeight - 50, 100])
+        return(valueAxis)
+}
+const modulePercentageAxis = (rangeHeight: number)=>{
+   let percentageAxis = d3
+    .scaleLinear()
+    .domain([0 , 100 ])
+    .range([rangeHeight - 50, 100]);
+    return(percentageAxis)
+}
+const moduleCategories = (pareto: Pareto)=>{
+    const paretoCategoryValues:string[] = [];
+    pareto.stackedBars.forEach((p) => {
+        paretoCategoryValues.push(p.label);
+    });
+    return(paretoCategoryValues)
+}
+const moduleTicks = (pareto: Pareto)=>{
+    let ticks:number = 0;
+    if(pareto.maxValue <= 50){
+        ticks = 5;
+    }else if (pareto.maxValue >50 && pareto.maxValue <= 100){
+        ticks= 10;
+    }else {
+        ticks= 20;
+    }
+    return(ticks)
+}
+export {moduleCategoryAxis,modulePercentageAxis,moduleValueAxis, moduleTicks,moduleCategories};
