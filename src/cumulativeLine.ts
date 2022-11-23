d7d34e075ea410b31acc9d4e65517cc6bea1dc8f
Mozand <81272931+Mozand@users.noreply.github.com>
2022-11-23 12:36:14 +0100

    Revert "#111 Use index for identifying stacked bar data"

--- src/cumulativeLine.ts
+++ src/cumulativeLine.ts
@@ -1,6 +1,6 @@
 import * as d3 from "d3";
 import {Pareto} from "./pareto";
-import{moduleCategoryAxis, moduleIndices, modulePercentageAxis} from "./axis"
+import{moduleCategoryAxis, moduleCategories, modulePercentageAxis} from "./axis"
 
 /**
  * Render the cumulative line using d3
@@ -8,17 +8,17 @@ import{moduleCategoryAxis, moduleIndices, modulePercentageAxis} from "./axis"
  */
 export function renderCumulativeLine(pareto: Pareto) {
 
-    const paretoCategoryIndices:number[] = moduleIndices(pareto)
+    const paretoCategoryValues:string[] = moduleCategories(pareto)
 
     let d3svg = d3.select("svg")
     const svg:any = document.querySelector("#svg");
     const svgBoundingClientRect:any = svg.getBoundingClientRect();
-    const categoryAxisBandwidth = moduleCategoryAxis(paretoCategoryIndices, 0, svgBoundingClientRect.width); //used to get bandwidth later
-    const categoryAxis = moduleCategoryAxis(paretoCategoryIndices, categoryAxisBandwidth.bandwidth()/2, svgBoundingClientRect.width + (categoryAxisBandwidth.bandwidth()/2));
+    const categoryAxisBandwidth = moduleCategoryAxis(paretoCategoryValues, 0, svgBoundingClientRect.width); //used to get bandwidth later
+    const categoryAxis = moduleCategoryAxis(paretoCategoryValues, categoryAxisBandwidth.bandwidth()/2, svgBoundingClientRect.width + (categoryAxisBandwidth.bandwidth()/2));
     const valueAxis = modulePercentageAxis(svgBoundingClientRect.height);
 
     const positions = pareto.stackedBars.map((stackedBar) => {
-        return [stackedBar.index, stackedBar.cumulativePercentage];
+        return [stackedBar.label, stackedBar.cumulativePercentage];
     })
 
     var line = d3.line<any>()
