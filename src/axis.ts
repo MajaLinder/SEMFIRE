cbb4224a499ac4e61be6c49bdc5942bbbf840557
Maja Linder <guskalmas@student.gu.se>
2022-11-25 10:49:37 +0100

    #113 Use prettier to format the code

--- src/axis.ts
+++ src/axis.ts
@@ -1,88 +1,89 @@
 import * as d3 from "d3";
-import { Pareto} from "./pareto";
+import { Pareto } from "./pareto";
 
- export function renderAxes(pareto: Pareto) {
+export function renderAxes(pareto: Pareto) {
     d3.selectAll("path").remove();
     d3.selectAll("g").remove();
 
-    const paretoCategoryValues:string[] = moduleCategories(pareto)
+    const paretoCategoryValues: string[] = moduleCategories(pareto);
 
-    let ticks=moduleTicks(pareto)
+    let ticks = moduleTicks(pareto);
     var d3svg = d3.select("svg");
-    const svg:any = document.querySelector("#svg");
-    const svgBoundingClientRect:any = svg.getBoundingClientRect();
+    const svg: any = document.querySelector("#svg");
+    const svgBoundingClientRect: any = svg.getBoundingClientRect();
 
     var d3svg = d3.select("svg");
-    const categoryAxis= moduleCategoryAxis(paretoCategoryValues, 0, svgBoundingClientRect.width);
-    const valueAxis = moduleValueAxis(pareto.maxValue,svgBoundingClientRect.height,ticks);
-    const percentageAxis =  modulePercentageAxis(svgBoundingClientRect.height)
-   
-    var g = d3svg
-        .append("g") 
-        .attr("transform", "translate(" + 65 + "," + 0 + ")");
-  
+    const categoryAxis = moduleCategoryAxis(paretoCategoryValues, 0, svgBoundingClientRect.width);
+    const valueAxis = moduleValueAxis(pareto.maxValue, svgBoundingClientRect.height, ticks);
+    const percentageAxis = modulePercentageAxis(svgBoundingClientRect.height);
+
+    var g = d3svg.append("g").attr("transform", "translate(" + 65 + "," + 0 + ")");
+
     g.append("g")
         .attr("transform", "translate(0," + (svgBoundingClientRect.height - 50) + ")")
-        .call(d3.axisBottom(categoryAxis).scale(categoryAxis))
-        
+        .call(d3.axisBottom(categoryAxis).scale(categoryAxis));
 
     g.append("g").call(d3.axisLeft(valueAxis).ticks(ticks));
     g.append("g")
         .attr("transform", "translate(" + (svgBoundingClientRect.width - 100) + " ,0)")
-        .call(d3.axisRight(percentageAxis).tickPadding(2).ticks(10)
-        .tickFormat(function(d) {
-          return d + "%";
-        }));
+        .call(
+            d3
+                .axisRight(percentageAxis)
+                .tickPadding(2)
+                .ticks(10)
+                .tickFormat(function (d) {
+                    return d + "%";
+                })
+        );
 }
 
-const moduleCategoryAxis =(domain:any, rangeStart:number , rangeWidth:number)=>{
-
+const moduleCategoryAxis = (domain: any, rangeStart: number, rangeWidth: number) => {
     let categoryAxis = d3
-            .scaleBand()
-            .domain(domain)           
-            .range([rangeStart, rangeWidth - 100])
-            .paddingInner(0.13)
-            .paddingOuter(0.26)
+        .scaleBand()
+        .domain(domain)
+        .range([rangeStart, rangeWidth - 100])
+        .paddingInner(0.13)
+        .paddingOuter(0.26);
     return categoryAxis;
-
-}
-const moduleValueAxis = (domain:any,rangeHeight:number,ticks:number)=>{
+};
+const moduleValueAxis = (domain: any, rangeHeight: number, ticks: number) => {
     let valueAxis = d3
-            .scaleLinear()
-            .domain([0, domain]).nice(ticks)
-            .range([rangeHeight - 50, 100])
-        return valueAxis;
-}
-const modulePercentageAxis = (rangeHeight: number)=>{
-   let percentageAxis = d3
-    .scaleLinear()
-    .domain([0 , 100 ])
-    .range([rangeHeight - 50, 100]);
+        .scaleLinear()
+        .domain([0, domain])
+        .nice(ticks)
+        .range([rangeHeight - 50, 100]);
+    return valueAxis;
+};
+const modulePercentageAxis = (rangeHeight: number) => {
+    let percentageAxis = d3
+        .scaleLinear()
+        .domain([0, 100])
+        .range([rangeHeight - 50, 100]);
     return percentageAxis;
-}
-const moduleCategories = (pareto: Pareto)=>{
-    const paretoCategoryValues:string[] = [];
+};
+const moduleCategories = (pareto: Pareto) => {
+    const paretoCategoryValues: string[] = [];
     pareto.stackedBars.forEach((p) => {
         paretoCategoryValues.push(p.label);
     });
     return paretoCategoryValues;
-}
-const moduleIndices = (pareto: Pareto)=>{
-    const paretoCategoryIndices:number[] = [];
+};
+const moduleIndices = (pareto: Pareto) => {
+    const paretoCategoryIndices: number[] = [];
     pareto.stackedBars.forEach((p) => {
         paretoCategoryIndices.push(p.index);
     });
     return paretoCategoryIndices;
-}
-const moduleTicks = (pareto: Pareto)=>{
-    let ticks:number = 0;
-    if(pareto.maxValue <= 50){
+};
+const moduleTicks = (pareto: Pareto) => {
+    let ticks: number = 0;
+    if (pareto.maxValue <= 50) {
         ticks = 5;
-    }else if (pareto.maxValue >50 && pareto.maxValue <= 100){
-        ticks= 10;
-    }else {
-        ticks= 20;
+    } else if (pareto.maxValue > 50 && pareto.maxValue <= 100) {
+        ticks = 10;
+    } else {
+        ticks = 20;
     }
     return ticks;
-}
-export {moduleCategoryAxis,modulePercentageAxis,moduleValueAxis, moduleTicks,moduleCategories, moduleIndices};
+};
+export { moduleCategoryAxis, modulePercentageAxis, moduleValueAxis, moduleTicks, moduleCategories, moduleIndices };
--- src/stackedBars.ts
+++ src/stackedBars.ts
@@ -1,31 +1,36 @@
 import * as d3 from "d3";
-import {Pareto} from "./pareto";
-import{moduleCategoryAxis,moduleValueAxis, moduleTicks,moduleCategories} from "./axis"
+import { Pareto } from "./pareto";
+import { moduleCategoryAxis, moduleValueAxis, moduleTicks, moduleCategories } from "./axis";
 /**
  * Render the bars using d3
  * @param pareto Pareto data structure
  */
- export function renderStackedBars(pareto: Pareto) {
-   
-    const paretoCategoryValues:string[] = moduleCategories(pareto)
+export function renderStackedBars(pareto: Pareto) {
+    const paretoCategoryValues: string[] = moduleCategories(pareto);
 
-    let ticks=moduleTicks(pareto)
-    const svg:any = document.querySelector("#svg");
-    const svgBoundingClientRect:any = svg.getBoundingClientRect();
-    const categoryAxis= moduleCategoryAxis(paretoCategoryValues, 0, svgBoundingClientRect.width);
-    const valueAxis = moduleValueAxis(pareto.maxValue,svgBoundingClientRect.height,ticks);
-   
+    let ticks = moduleTicks(pareto);
+    const svg: any = document.querySelector("#svg");
+    const svgBoundingClientRect: any = svg.getBoundingClientRect();
+    const categoryAxis = moduleCategoryAxis(paretoCategoryValues, 0, svgBoundingClientRect.width);
+    const valueAxis = moduleValueAxis(pareto.maxValue, svgBoundingClientRect.height, ticks);
+
     var d3svg = d3.select("svg");
-    let g =d3svg.select("g")
+    let g = d3svg.select("g");
 
     g.selectAll(".bar")
-         .data(pareto.stackedBars)
-         .enter().append("rect")
-         .attr("fill", "pink")
-         .attr("class", "bar")
-         .attr("x", function(d):any { return categoryAxis(d.label); })
-         .attr("y", function(d):any { return valueAxis(d.totalValue); })
-         .attr("width", categoryAxis.bandwidth())
-         .attr("height", function(d) { return ((svgBoundingClientRect.height -50) - Number (valueAxis (d.totalValue))) });
-
+        .data(pareto.stackedBars)
+        .enter()
+        .append("rect")
+        .attr("fill", "pink")
+        .attr("class", "bar")
+        .attr("x", function (d): any {
+            return categoryAxis(d.label);
+        })
+        .attr("y", function (d): any {
+            return valueAxis(d.totalValue);
+        })
+        .attr("width", categoryAxis.bandwidth())
+        .attr("height", function (d) {
+            return svgBoundingClientRect.height - 50 - Number(valueAxis(d.totalValue));
+        });
 }
