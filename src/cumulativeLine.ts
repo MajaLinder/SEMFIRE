5ad39485678ca0036229b67c32ffcd07624d0019
LIYUHAN22 <liyuhan20000321@gmail.com>
2022-12-12 15:14:16 +0100

    #161 Change the magic number to store in variables

--- src/cumulativeLine.ts
+++ src/cumulativeLine.ts
@@ -50,6 +50,7 @@ export function renderCumulativeLine(pareto: Pareto) {
 
         .style("fill", "#3050EF");
 
+    const lineWeight = 3;
     d3svg
         .append("path")
         .datum(positions)
@@ -58,5 +59,5 @@ export function renderCumulativeLine(pareto: Pareto) {
         .attr("d", line)
         .style("fill", "none")
         .style("stroke", "#3050EF")
-        .style("stroke-width", 3);
+        .style("stroke-width", lineWeight);
 }
